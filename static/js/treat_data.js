c2b95234d9160ca486461c0a00bbaa260c7c13b7
FumitakaIwaki <iwaki2323a@gmail.com>
2023-01-28 15:09:29 +0900

    調整

--- static/js/treat_data.js
+++ static/js/treat_data.js
@@ -1,6 +1,9 @@
 function get_value() {
   let data = {}
 
+  const cnt = document.getElementById("counter");
+  data['qnum'] = "q" + ((cnt.value % 2) + 1)
+
   elements = document.getElementsByName('jender');
   let len = elements.length;
   for (let i = 0; i < len; i++){
@@ -8,7 +11,7 @@ function get_value() {
         data['jender'] = elements.item(i).value;
     }
   }
-
+  
   data['age'] = document.getElementById('age').value;
   data['ex1'] = document.getElementById('ex1').value;
   data['ex2'] = document.getElementById('ex2').value;
