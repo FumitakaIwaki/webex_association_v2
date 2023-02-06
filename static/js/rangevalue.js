168d286a62a210e5f1560d0de8a0eb480f5db128
FumitakaIwaki <iwaki2323a@gmail.com>
2023-02-06 18:32:09 +0900

    is_answer調整

--- static/js/rangevalue.js
+++ static/js/rangevalue.js
@@ -7,19 +7,22 @@ const assoc_label = [0, 1, 2, 3, 4, 5, 6, 7]
 // E*(n-1) ~ E*n までのelemを調べる
 function is_answer(n) {
   let En = Math.min(E*n, N);
+  let Flag = true;
   for (let i=E*(n-1); i < En; i++) {
     // 全て回答しているかどうか
     if (choicedValueElem[i].innerText == "回答してください") {
       alert("回答していない質問があります");
+      Flag = false;
       break;
+    }
+  }
+  if (Flag) {
+    if (n != G) {
+      // 次のページへ
+      next_div(k=n);
     }else{
-      if (n != G) {
-        // 次のページへ
-        next_div(k=n);
-      }else{
-        // 最後のページの場合、回答送信
-        get_value();
-      }
+      // 最後のページの場合、回答送信
+      get_value();
     }
   }
 }
